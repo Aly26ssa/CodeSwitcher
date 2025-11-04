b2f8faa22ebf76f6d5dc23b94b42ff60c7027e79
Alyssa Vogt <AlyssaVogt2003@gmail.com>
2025-11-03 19:06:41 -0500

    editing the css

--- JS/script.js
+++ JS/script.js
@@ -6,20 +6,17 @@ document.addEventListener("DOMContentLoaded", function() {
         tab.addEventListener("click", function() {
             const language = this.getAttribute("data-language");
 
-            // Remove active class from all tabs and code blocks
             tabs.forEach(t => t.classList.remove("code-container__tab--active"));
             codeBlocks.forEach(c => c.classList.remove("code-container__code--active"));
 
-            // Add active class to the clicked tab and corresponding code block
             this.classList.add("code-container__tab--active");
             document.querySelector(`.code-container__code--${language}`).classList.add("code-container__code--active");
 
-            // Re-highlight the code block for Prism.js
             Prism.highlightElement(document.querySelector(`.code-container__code--${language} code`));
         });
     });
 
-// Footer Animation
+// Footer
 
 const footer = document.querySelector('.footer__inner');
 const footerSpans = footer.querySelectorAll('span');
@@ -29,9 +26,9 @@ const observer = new IntersectionObserver(entries => {
                 footerSpans.forEach((span, index) => {
                     setTimeout(() => {
                         span.classList.add('animate');
-                    }, index * 100); // Delay each letter by 100ms
+                    }, index * 100);
                 });
-                observer.unobserve(footer); // Unobserve after animation triggers
+                observer.unobserve(footer);
             }
         });
     }, {
